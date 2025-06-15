16a24d106d8718e45073fc88e30f771193c7f904
Tocacreamy <zekobaharudinfirdaus@gmail.com>
2025-06-15 16:05:37 +0700

    fix route , using ../

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,11 +1,11 @@
-import HomePage from "../../pages/home/home-page.js";
-import Register from "../../pages/register/regist-page.js";
-import Login from "../../pages/login/login-page.js";
-import UploadStoryPage from "../../pages/uploadStory/uploadStory-page.js";
-import DetailStory from "../../pages/detailStory/detailStory-page.js";
-import NotificationsPage from "../../pages/notifications/notifications-page.js";
-import SavedStoriesPage from "../../pages/savedStories/savedStories-page.js";
-import NotFoundPage from "../../pages/notFound/notFound-page.js";
+import HomePage from "../pages/home/home-page.js";
+import Register from "../pages/register/regist-page.js";
+import Login from "../pages/login/login-page.js";
+import UploadStoryPage from "../pages/uploadStory/uploadStory-page.js";
+import DetailStory from "../pages/detailStory/detailStory-page.js";
+import NotificationsPage from "../pages/notifications/notifications-page.js";
+import SavedStoriesPage from "../pages/savedStories/savedStories-page.js";
+import NotFoundPage from "../pages/notFound/notFound-page.js";
 
 const routes = {
   "/": new HomePage(),
